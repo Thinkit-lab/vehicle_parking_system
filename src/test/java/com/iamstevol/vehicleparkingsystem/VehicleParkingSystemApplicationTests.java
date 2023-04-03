37365cf37fd92d9b11f198b3f864a5fa96db6fb5
Thinkit-lab <afolabikunle2@gmail.com>
2023-04-03 17:02:15 +0100

    CHORE: Configure maven.yml for CI/CD

--- src/test/java/com/iamstevol/vehicleparkingsystem/VehicleParkingSystemApplicationTests.java
+++ src/test/java/com/iamstevol/vehicleparkingsystem/VehicleParkingSystemApplicationTests.java
@@ -1,15 +1,15 @@
-//package com.iamstevol.vehicleparkingsystem;
-//
-//import org.junit.jupiter.api.Disabled;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class VehicleParkingSystemApplicationTests {
-//
-//    @Test
-//    void demo_test() {
-//
-//    }
-//
-//}
+package com.iamstevol.vehicleparkingsystem;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleParkingSystemApplicationTests {
+
+    @Test
+    void demo_test() {
+
+    }
+
+}
